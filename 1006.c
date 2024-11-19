10007780ef1e0b264837be8222b6ba319532a7cf
RonaldoVieira05 <ronaldo.vieira@unifesp.br>
2024-11-19 14:55:46 -0300

    Create 1006.c

---
+++ 1006.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    float A, B, C, MEDIA;
+ 
+    scanf("%f", &A);
+    scanf("%f", &B);
+    scanf("%f", &C);
+    MEDIA = ((2 * A) + (3 * B) + (5 * C)) / 10;
+    printf("MEDIA = %.1f\n", MEDIA);
+    return 0;
+}
